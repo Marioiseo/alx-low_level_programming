f7e92d2c1e71eee3e3c70f94173d04303f33e27e
marioiseo <mariohany302@gmail.com>
2023-03-16 03:16:28 -0700

    headache

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 /**
 	*main function - puts first shot
+	* Description: This is a longer description.
+	* Don't forget that a line should not exceed 80 characters.
+	* But you're totally free to use several lines to properly
 	* describe your function
 	* Return: The sum of the two parameters
 */
