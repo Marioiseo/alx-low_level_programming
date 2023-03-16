11a819c90509f7c2adc8672d84fb83d2ce31416a
marioiseo <mariohany302@gmail.com>
2023-03-16 03:11:21 -0700

    useless

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
  * main function - puts first shot
+ *
  * used to output chars in the main function
  * return value of main function  
  * function main is the main function -_-
