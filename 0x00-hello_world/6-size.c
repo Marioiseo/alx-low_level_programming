70538832199a4d1369d985c040917726d41e8436
marioiseo <mariohany302@gmail.com>
2023-03-16 04:28:53 -0700

    lol

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 *main - sizeof to declare every byte of ram
-*return: 0 (success)
+*Return: 0 (success)
 */
 int main(void)
 	{
@@ -11,11 +11,11 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("Size of a char: %Iu Byte(s)\n", sizeof(a));
-	printf("Size of a int: %Iu Byte(s)\n", sizeof(b));
-	printf("Size of a long int: %Iu Byte(s)\n", sizeof(c));
-	printf("Size Of a long long int: %Iu Byte(s)\n", sizeof(d));
-	printf("Size of a float: %Iu Byte(s)\n", sizeof(e));
+	printf("Size of a char: %Iu byte(s)\n", sizeof(a));
+	printf("Size of a int: %Iu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %Iu byte(s)\n", sizeof(c));
+	printf("Size Of a long long int: %Iu byte(s)\n", sizeof(d));
+	printf("Size of a float: %Iu byte(s)\n", sizeof(e));
 
 	return (0);
 	}
