7afd011f5422edfefbe13cbd72bec99547f7a7ac
marioiseo <mariohany302@gmail.com>
2023-03-16 04:05:56 -0700

    lastone

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - sizeof to declare every byte of ram
+*Return : 0 (success)
+*/
+int main(void)
+	{
+	printf(sizeof(char));
+	printf(sizeof(int));
+	printf(sizeof(long int));
+	printf(sizeof(long long int));
+	printf(sizeof(float));
+
+	return (0);
+	}
