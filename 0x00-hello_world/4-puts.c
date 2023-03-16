6147f280291715ec1d187d99663760ad5e2946d9
marioiseo <mariohany302@gmail.com>
2023-03-16 03:12:37 -0700

    hell

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main function - puts first shot
- *
- * used to output chars in the main function
- * return value of main function  
- * function main is the main function -_-
- */
+*main function - puts first shot
+*
+*used to output chars in the main function
+*return value of main function  
+*function main is the main function -_-
+*/
 int main(void)
 	{
 	char myoput[] = "\"Programming is like building a multilingual puzzle";
