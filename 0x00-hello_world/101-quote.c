c2670800ce64a315d8872540a64335913e5f4e73
marioiseo <mariohany302@gmail.com>
2023-03-16 08:52:58 -0700

    this is useless

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
-/**
- * prcout - using printf finally
- * Return: 1 " Success "
- */
-int prcout()
-	{
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
-	}
+#include <unistd.h>
 /**
  * main - calling the function prcout
  * Return: 0 " success "
  */
 int main(void)
 	{
-	prcout();
-	return (0);
+	char stri[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	
+	write(1,stri,59);
+	return (1);
 	}
