ff57874ee38c6109e0cca937018c9b4938a9e629
marioiseo <mariohany302@gmail.com>
2023-03-16 03:37:07 -0700

    now we got it

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,9 @@
  */
 int main (void)
 	{
-	int printf (with proper grammar, but the outcome is a piece of art,);
+	printf ( "with proper grammar, but the outcome is a piece of art," );
+
 
 	return (0);
+
 	}
