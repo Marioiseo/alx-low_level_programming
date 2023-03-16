a31d23d244ffe80c021036eb5d05a688566b6057
marioiseo <mariohany302@gmail.com>
2023-03-16 08:34:48 -0700

    lets try this

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,9 @@
  * prcout - using printf finally
  * Return: 1 " Success "
  */
-int prcout(void)
+int prcout(int)
 	{
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 	}
 /**
