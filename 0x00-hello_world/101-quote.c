507c5891a0f6f9ab4604a39299fcfca8f8b7c201
marioiseo <mariohany302@gmail.com>
2023-03-16 08:56:14 -0700

    finally

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,10 @@
 /**
  * main - calling the function prcout
  * Return: 0 " success "
- */
+*/
 int main(void)
 	{
 	char stri[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
-	write(1,stri,59);
+	write(1, stri, 59);
 	return (1);
 	}
