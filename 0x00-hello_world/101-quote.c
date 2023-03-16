428bedad025e1774171ff2f9c128d3df26d8ec85
marioiseo <mariohany302@gmail.com>
2023-03-16 08:24:47 -0700

    first  try

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+using namespace std;
+/**
+ * main - using cout finally
+ * Return: 1 " Success "
+ */
+int prcout(void)
+	{
+	cout << "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	return (1);
+	}
+int main(void)
+	{
+	prcout();
+	return (0);
+	}
+	
