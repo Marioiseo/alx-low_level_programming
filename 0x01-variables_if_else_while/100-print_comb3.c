dc14e02b012658ff34f78c9c92674758a0d1fe35
marioiseo <mariohany302@gmail.com>
2023-03-17 09:35:42 -0700

    YEES git add .

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,7 +11,7 @@ int main(void)
 
 	int b;
 
-	for (b = 0; b <= 9;)
+	for (b = 0; b <= 9; b++)
 	{
 	if (a == b)
 	{
@@ -21,11 +21,10 @@ int main(void)
 	putchar(b + '0');
 	putchar(',');
 	putchar(' ');
-	b++;
 	if (b == 9)
 	{
 	a++;
-	b = (a + 1);
+	b = (a);
 	}
 	}
 	putchar('\n');
