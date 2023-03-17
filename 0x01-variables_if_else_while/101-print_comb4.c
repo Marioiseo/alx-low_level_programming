efd2aabe0bc7f8b7b22b3bfd37acdba0ea230483
marioiseo <mariohany302@gmail.com>
2023-03-17 09:56:14 -0700

    hard idea

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - 3 more combinations to go
+ * Return: 0 As always
+*/
+int main(void)
+	{
+	int a = 0;
+
+	int b = 1;
+
+	int c;
+
+	for (c = 2; c <= 9; c++)
+	{
+	if (a == b && a == c && b == c )
+	{
+		continue;
+	}
+	putchar(a + '0');
+	putchar(b + '0');
+	putchar(c + '0');
+	if (a == 7)
+	continue;
+	putchar(',');
+	putchar(' ');
+	if (c == 9)
+	{
+	a++;
+	b++;
+	c = (b);
+	}
+	}
+	putchar('\n');
+	return (0);
+	}
