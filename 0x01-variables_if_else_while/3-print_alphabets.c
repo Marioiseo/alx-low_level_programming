a066fffd196b4efc8ca32e9ba0c5bbb40b761402
marioiseo <mariohany302@gmail.com>
2023-03-17 06:25:11 -0700

    slightly missunderstood

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,8 +8,15 @@ int main(void)
         char c;
 
 	char b;
+
         for (c = 'a'; c <= 'z'; c++)
+	{
         putchar(c);
+	}
+	for (b = 'A'; b <= 'Z'; b++)
+	{
+	putchar(b);
 	putchar('\n');
+	}
         return (0);
         }
