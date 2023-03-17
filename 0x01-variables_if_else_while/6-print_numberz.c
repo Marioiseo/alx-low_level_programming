6282edb089dbb0967335c65addc4f72597b05af3
marioiseo <mariohany302@gmail.com>
2023-03-17 06:42:51 -0700

    truly hateful for betty

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,11 +4,11 @@
  * Return: "0" success
 */
 int main(void)
-        {
-        int c;
+	{
+	int c;
 
-        for (c = 0 ; c <= 9; c++);
-        putchar('0' + c);
+	for (c = 0; c <= 9; c++);
+	putchar(c + '0');
 	putchar ('\n');
-        return (0);
-        }
+	return (0);
+	}
