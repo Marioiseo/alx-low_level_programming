71ddc9e18644542cb6a7163365302e685b54da88
marioiseo <mariohany302@gmail.com>
2023-03-21 14:41:55 -0700

    getting harder now

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,15 +16,16 @@ void times_table(void)
 	{
 	_putchar((c / 10) + '0');
 	_putchar((c % 10) + '0');
+	_putchar(',');
 	}
 	else if (c <= 9)
 	{
 	_putchar(c + '0');
+	_putchar(',');
+	_putchar(' ');
 	}
 	if (b <9)
 	{
-	_putchar(',');
-	_putchar(' ');
 	_putchar(' ');
 	}
 	}
