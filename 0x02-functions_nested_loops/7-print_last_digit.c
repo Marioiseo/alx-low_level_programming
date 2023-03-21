33c4f0482fcddcad5b0955792abb3c92452dbcdd
marioiseo <mariohany302@gmail.com>
2023-03-21 12:15:12 -0700

    yuppy

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - name is specific
+ * @c: checker
+ * Return: maybe 1 maybe 0
+*/
+int print_last_digit(int c)
+{
+	int b;
+
+	b = (c % 10);
+	if ( b < 0)
+	{
+	b = (b * -1);
+	}
+	return (b);
+}
