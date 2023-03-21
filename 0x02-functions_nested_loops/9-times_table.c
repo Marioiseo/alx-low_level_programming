714b08ce823a01ed6f68bfbb647e518f9140149b
marioiseo <mariohany302@gmail.com>
2023-03-21 13:12:32 -0700

    here we go

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * times_table - all we have here is voooid !
+ * Return: void only returns void
+*/
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+	for (b =0; b <=9; b++)
+	{
+	c = ( a * b );
+	_putchar(c + '0');
+	_putchar(',');
+	_Putchar(' ');
+	_putchar(' ');
+	}
+	_putchar('\n');
+	}
+}
