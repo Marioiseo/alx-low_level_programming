5b87f5546fae60f0a35077afd0540395e1252f5e
marioiseo <mariohany302@gmail.com>
2023-03-21 11:27:40 -0700

    nested is easy

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - make it nested
+ * Return: void
+*/
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+		_putchar(c);
+		}
+	_putchar('\n')
+	}
