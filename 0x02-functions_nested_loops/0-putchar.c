e8664dd809d8439e575127bd509d2ed1c5a26d74
marioiseo <mariohany302@gmail.com>
2023-03-21 10:47:41 -0700

    its hard

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,18 @@
-#include <unistd.h>
+#include "main.h"
 /**
- * _putchar - write the char 
- * Return: On Success 1.
+ *main - Entry point
+ * Return: Always 0
 */
-int _putchar(char c)
-	{
-	return (write(1, &c , 1));
-	}
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
