9be625475ba786cb909561912e444e2199a43b5c
marioiseo <mariohany302@gmail.com>
2023-03-21 14:30:22 -0700

    so close

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,10 +12,14 @@ void times_table(void)
 	for (b = 0; b <= 9; b++)
 	{
 	c = (a * b);
-	_putchar(c + '0');
+	_putchar((c / 10) + '0');
+	_putchar((c % 10) + '0');
+	if (b <9)
+	{
 	_putchar(',');
 	_putchar(' ');
 	_putchar(' ');
+	}
 	}
 	_putchar('\n');
 	}
