192cb706637cdae8b314565d56e00d126b118212
marioiseo <mariohany302@gmail.com>
2023-03-21 14:55:11 -0700

    thats my limit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,27 +7,26 @@ void times_table(void)
 {
 	int a, b, c;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 1; a <= 9; a++)
 	{
-	for (b = 0; b <= 9; b++)
+	_putchar(48);
+	for (b = 1; b <= 9; b++)
 	{
 	c = (a * b);
+	if (b < 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+	if (c <= 9)
+	{
+	_putchar(' ')
+	_putchar(c + '0');
+	}
 	if (c > 9)
 	{
-	_putchar((c / 10) + '0');
-	_putchar((c % 10) + '0');
-	_putchar(',');
-	}
-	else if (c <= 9)
-	{
-	_putchar(c + '0');
-	_putchar(',');
-	_putchar(' ');
-	}
-	if (b <9)
-	{
-	_putchar(' ');
-	}
+	_putchar((c / 10) + '0')
+	_putchar((c % 10) + '0')
 	}
 	_putchar('\n');
 	}
