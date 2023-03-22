d31b32731402d4eab6675554a6a1b8bcbbe7d43c
marioiseo <mariohany302@gmail.com>
2023-03-22 04:58:00 -0700

    i wish

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,20 +8,30 @@ void print_to_98(int n)
 {
 	int a, b;
 
-	while (n != 98)
+	if (n != 98 && n < 98)
 	{
-		for (a = n; a <= 98; a++)
-		{
-			_putchar(a + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-	for (b = n; b > 98; b--)
+	a = n;
+	for (a != 98; a < 98; a++)
 	{
-		_putchar(b + '0');
-		_putchar(',');
-		_putchar(' ');
+	_putchar(a + '0');
+	_putchar(',');
+	_putchar(' ');
 	}
-	_putchar('\n');
+	if (n != 98 && n > 98)
+	{
+	b = n;
+	for (b != 98; b > 98; b--)
+	{
+	_putchar(b + '0');
+	_putchar(',');
+	_putchar(' ');
+	}
+	if (n = 98)
+	{
+	_putchar(n + '0');
+	_putchar(',');
+	_putchar(' ');
+	}
+	}
 	}
 }
