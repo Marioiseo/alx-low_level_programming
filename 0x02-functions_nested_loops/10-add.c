564e58e6e697ab7c81b506acfd94bc6dc81d7fde
marioiseo <mariohany302@gmail.com>
2023-03-22 04:34:40 -0700

    easy

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - its easy for me to add ints
+ * @a: first num
+ * @b: second num
+ * Return: c because i am always right ^_^
+*/
+int add(int a, int b)
+{
+	int c;
+
+	c = (a + b);
+	return (c);
+}
