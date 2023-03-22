a94055c2837d0715c7da91a1379c1ded8bf7d430
marioiseo <mariohany302@gmail.com>
2023-03-22 09:08:39 -0700

    i dont think so

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_times_table - lovely rough task
  * @n: more than 0 less than 15
@@ -8,7 +9,7 @@ void print_times_table(int n)
 {
 	int a, b, c;
 
-	for (n >= 0; n <= 15;)
+	if (n >= 0 && n <= 15)
 	{
 	for (a = 0; a <= n; a++)
 	{
