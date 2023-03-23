5f0dc4f28208097df5ce442c2ccccaf0ef9a4368
Marioiseo <mariohany302@gmail.com>
2023-03-23 12:24:23 -0700

    easier and easier

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - lets use putchar now
+ * Return: vooid is vooid ^_^
+*/
+void print_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
