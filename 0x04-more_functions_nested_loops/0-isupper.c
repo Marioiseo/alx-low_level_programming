efc0ffb352d09032db857e5f2891892767534bee
marioiseo <mariohany302@gmail.com>
2023-03-23 11:56:15 -0700

    easy

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - for this we gonna recognize the upper cases with ASCII
+ * Return: either 0 or 1 
+*/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
