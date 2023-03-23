b03e5904f829aa60086c0a0d939530921287b7b5
Marioiseo <mariohany302@gmail.com>
2023-03-23 12:20:54 -0700

    easy

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiple is easy for us now
+ * @a: the num 1
+ * @b: the num 2
+ * Return: the value
+*/
+int mul(int a, int b)
+{
+	int c;
+
+	c = (a * b);
+	return (c);
+}
