f551e10ab420ff0602d830b024c7010418c47122
Marioiseo <mariohany302@gmail.com>
2023-03-24 01:53:03 -0700

    fizzy fizzy thanks to elzero

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - i love this task LOL <3
+ * Return: maybe void maybe 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+	if ((i % 3) = 0)
+		printf("Fizz");
+	else if ((i % 5) = 0)
+		printf("Buzz");
+	else if (((i % 3) = 0) && ((i % 5) = 0))
+		printf("FizzBuzz");
+	else
+	printf("%i", i);
+	}
+}
