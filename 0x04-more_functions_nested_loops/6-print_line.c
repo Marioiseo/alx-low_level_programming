be3c4edcc2dbf74e53403f0bf51e059d0b4f14c2
Marioiseo <mariohany302@gmail.com>
2023-03-24 00:46:31 -0700

    easy nested

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - this is the function for just lines
+ * @n: nums of ____>><<
+ * Return: VOOID
+*/
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		int i;
+
+		for (i = 1; i = n; i++)
+		_putchar('_');
+	}
+	_putchar('\n');
+}
