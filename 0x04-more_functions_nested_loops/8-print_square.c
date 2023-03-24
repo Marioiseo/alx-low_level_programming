73ce21803d04412616dc48ddf9452e1001458b97
Marioiseo <mariohany302@gmail.com>
2023-03-24 01:43:59 -0700

    easy

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - thats more easily than the previous one lol
+ * @size: the i * j = size
+ * Return: VOOID forever ^_^
+*/
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	while (size > 0)
+	{
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
+}
