aa63800c5e4b62b83fb13892c4a9890cacb6adf6
Marioiseo <mariohany302@gmail.com>
2023-03-24 01:25:27 -0700

    some mistakes are bugs some are stupidity

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,17 +12,15 @@ void print_diagonal(int n)
 	{
 		for (i = 1; i <= n; i++)
 		{
-		if (i == 1)
-			_putchar('\\');
-		}
 		for (j = 1; j < i; j++)
 		{
-		_putchar(' ');
-		_putchar('\\');
+		_putchar(' ');;
 		}
+		_putchar('\\'_;
 		_putchar('\n');
 		if (i == n)
 			break;
+		}
 	}
 	_putchar('\n');
 }
