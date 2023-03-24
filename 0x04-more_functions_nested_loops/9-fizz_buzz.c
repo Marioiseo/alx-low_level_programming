6e06339545f987829d4d2062f79be4911ce8750c
Marioiseo <mariohany302@gmail.com>
2023-03-24 02:03:18 -0700

    this is insane git add .

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,13 +10,15 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 	if ((i % 3) == 0)
-		printf("Fizz ");
+		printf("Fizz");
 	else if ((i % 5) == 0)
-		printf("Buzz ");
-	else if ((i % 15) == 0)
-		printf("FizzBuzz ");
+		printf("Buzz");
+	else if (((i % 3) == 0) && ((i % 5) == 0))
+		printf("FizzBuzz");
 	else
 	printf("%i ", i);
+	if (i < 100)
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
