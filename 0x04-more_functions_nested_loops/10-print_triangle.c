407405c3a4306f6077d5d6be47925c6e2ada3a8a
Marioiseo <mariohany302@gmail.com>
2023-03-24 02:18:38 -0700

    expert at shapes

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_triangle - more shapes is easy ^_^
+ * @size: i * j = size <.>
+ * Return: VOID >><<
+*/
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	_putchar('\n');
+	while (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+		for (j = size; j > i; j--)
+		{
+			_putchar(' ');
+		}
+		_putchar('#');
+		_putchar('\n');
+		}
+		if (i == size)
+		break;
+	}
+}
