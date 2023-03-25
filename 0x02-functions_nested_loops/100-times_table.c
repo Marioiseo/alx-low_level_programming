f2f33aae6d3b862c8766a4dfdc088c1086951fe1
Marioiseo <mariohany302@gmail.com>
2023-03-25 08:24:46 -0700

    lets catch what we missed

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,17 +9,20 @@ void print_times_table(int n)
 {
 	int a, b;
 
-	if (n >= 0 && n <= 15)
+	while (n >= 0 && n <= 15)
+	{
 	for (a = 0; a <= n; a++)
 	{
 	for (b = 0; b <= n; b++)
 	{
-	if (b < n)
-	printf("%d,   ", a * b);
-	else
-	printf("%i", a * b);
+	if (b < 10)
+	printf("%i,   ", a * b);
+	else if (b >= 10)
+	printf("%i,  ", a * b);
+	else if (b == n)
+		printf("%i", a * b);
 	}
 	printf("\n");
 	}
+	}
 }
-
