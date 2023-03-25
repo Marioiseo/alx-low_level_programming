9d4c3e571edb2cde8a2f5014bc8447cdc2f40063
Marioiseo <mariohany302@gmail.com>
2023-03-25 08:46:03 -0700

    while making a troubles

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 {
 	int a, b;
 
-	while (n >= 0 && n <= 15)
+	if (n >= 0 && n <= 15)
 	{
 	for (a = 0; a <= n; a++)
 	{
@@ -24,7 +24,5 @@ void print_times_table(int n)
 	}
 	printf("\n");
 	}
-	if (a == n)
-		break;
 	}
 }
