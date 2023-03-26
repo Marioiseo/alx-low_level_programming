923154584667f65b21735777f7e7c9272b9c723e
Marioiseo <mariohany302@gmail.com>
2023-03-26 09:17:13 -0700

    wish its that easy

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - thats a lovely task to add the result to variable
+ * Return: void
+ */
+void main(void)
+{
+	int result = 0;
+
+	for (i = 0; i <= 1024; i++)
+	{
+		if((i % 3) == 0)
+			result += i;
+		else if ((i % 5) == 0)
+			result += i;
+	}
+	printf("%d\n", result);
+}
