bc919abd6007a9f145e78bf080eba01b745af7db
Marioiseo <mariohany302@gmail.com>
2023-03-27 09:40:24 -0700

    trying my best on this pointers l-l

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - same one without putchar 
+ * @s: same same 
+ * Return: void again
+*/
+void rev_string(char *s)
+{
+	int c = 0;
+
+	char a;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (c -= 1; c >= 0; c--)
+	{
+	a = s[c];
+	}
+	*s = a;
+}
