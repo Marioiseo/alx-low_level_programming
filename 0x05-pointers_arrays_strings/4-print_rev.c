42f0bbdbd75b44fc2960000a802cf8a3f0a41655
Marioiseo <mariohany302@gmail.com>
2023-03-27 08:38:54 -0700

    MAN THATS TOO EASY ^^

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - reversed is desrever
+ * @s: put your fav string please ^_^
+ * Return: void is void babe
+*/
+void print_rev(char *s)
+{
+	char i = *s;
+
+	for (i = '\0';i >= s[0]; i--)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+}
