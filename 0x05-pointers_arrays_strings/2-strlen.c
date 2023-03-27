b09d28751d31ce3562d02fa6433599c26a677a49
Marioiseo <mariohany302@gmail.com>
2023-03-27 02:46:14 -0700

    kinda easy

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - doing it without standard stdio is rough
+ * @s: lets hope its useful input
+ * Return: int i guess the num of chars
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+	*s++;
+	i++;
+	}
+	return (i);
+}
+	
