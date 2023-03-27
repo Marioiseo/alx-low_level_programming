80adb1b1259d22e3a9026e8ae441c0ade201c1b0
Marioiseo <mariohany302@gmail.com>
2023-03-27 02:28:54 -0700

    its really that easy

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - pointer as a param
- * @*n: pointer to the value need to be reset
+ * @n: pointer to the value need to be reset
  * Return: Void
 */
 void reset_to_98(int *n)
