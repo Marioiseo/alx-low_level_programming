8ce3759933f98e4d448a594c7867638270d4c2a1
Marioiseo <mariohany302@gmail.com>
2023-03-28 03:58:55 -0700

    wewaa

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,7 +7,7 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-	int a, b;
+	int a, b = 0;
 
 	while (src[a] != '\0')
 	{
@@ -16,7 +16,6 @@ char *_strcpy(char *dest, char *src)
 	for (b = 0; b <= a; b++)
 	{
 	dest[b] = src[b];
-	_putchar(dest[b]);
 	}
 	return (dest);
 }
