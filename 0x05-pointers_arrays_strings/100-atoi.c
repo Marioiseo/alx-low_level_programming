85d8069c4a37d31926372aa100baf75e6664db97
Marioiseo <mariohany302@gmail.com>
2023-03-28 08:26:44 -0700

    non sense

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _atoi - being advanced is rough
+ * @s: please enter the string here ^^
+ * Return: they call me int for a reason
+*/
+int _atoi(char *s)
+{
+	unsigned int num;
+
+	int i = 1;
+
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+	if (s[a] == '-')
+		i = i * -1;
+	else if (s[a] >= '0' && s[a] >= '9')
+		num = (num * 10) + (s[a] - 48);
+	else 
+		break;
+	a++;
+	}
+	return (num * i);
+}
