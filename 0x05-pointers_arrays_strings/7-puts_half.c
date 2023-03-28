77b756bb059a533f4f9759fc13e189b4c4da5727
Marioiseo <mariohany302@gmail.com>
2023-03-28 02:32:03 -0700

    new for me

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts_half - pointers are funny not hard ^^
+ * @str: please enter your string <3
+ * Return: void isnt that empty you know lol.
+*/
+void puts_half(char *str)
+{
+	int a = 0;
+
+	int fmiddle, lmiddle;
+
+	while (*str != '\0')
+	{
+		a++;
+	}
+	fmiddle = a / 2;
+	for (lmiddle = fmiddle; lmiddle <= a; lmiddle++)
+	{
+		_putchar(str[lmiddle]);
+	}
+	if
+		((lmiddle % 2) != 0);
+{
+	lmiddle = lmiddle + 1;
+		_putchar(str[lmiddle]);
+}
+	_putchar('\n');
+}
+
