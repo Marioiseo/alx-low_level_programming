87cd7a81aeb1f8c2e024123a0664ffc38894d0cb
Marioiseo <mariohany302@gmail.com>
2023-03-28 03:29:06 -0700

    creepy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcpy - thats insane
+ * @dest: please enter your lovely string
+ * @src: the copied one or vesce vesca
+ * Return: CHAR
+*/
+char *_strcpy(char *dest, char *src);
+{
+	&dest = &src;
+	return (*dest);
+}
