49d5fe03f69546a44bfee0a5adaf7b5664d98760
Marioiseo <mariohany302@gmail.com>
2023-03-30 10:24:33 -0700

    wewaa

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,10 +10,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (src[i] <= src[n])
+	while (i < n && src[i] != '\0')
 	{
 	dest[i] = src[i];
 		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++
 	}
 	return (dest);
 }
