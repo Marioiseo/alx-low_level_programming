7f97e7de855f7a4065b7f92ff55bc8f7f7508e27
Marioiseo <mariohany302@gmail.com>
2023-03-30 11:06:09 -0700

    lot of reversing i knew lot about it

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - name is pointing what it is ^_^
+ * @a: pointer or array or both
+ * @n: num of int upon it
+ * Return: void it is
+*/
+void reverse_array(int *a, int n)
+{
+	int *b;
+
+	int c = 0;
+
+	int i;
+
+	for (i = n; i >= 0; i--)
+	{
+	b[c] = a[i];
+	c++;
+	}
+}
