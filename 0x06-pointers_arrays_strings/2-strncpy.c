84468e620d4086a9c099d254c6984cf0c7c40255
Marioiseo <mariohany302@gmail.com>
2023-03-30 10:05:33 -0700

    checker is helpful

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncpy - copying with specific num count
+ * @dest: the copied one
+ * @src: the original one
+ * @n: nums of chars to copy
+ * Return: string char*
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != src[n])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
