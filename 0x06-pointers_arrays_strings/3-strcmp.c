2c3fff1e3a90d6e7d93e96b663ed95f15bbd73d2
Marioiseo <mariohany302@gmail.com>
2023-03-30 10:48:49 -0700

    maybe

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,18 +14,12 @@ int _strcmp(char *s1, char *s2)
 	for (i = 0; ; i++)
 	{
 	if (s1[i] == '\0' && s2[i] == '\0')
-	{
 	return (0);
-	}
 	a1 = (int) s1[i];
 	a2 = (int) s2[i];
-	if (a1 > a2)
-	{
+	else if (a1 > a2)
 	return (15);
-	}
-	if (a2 > a1)
-	{
+	else if (a2 > a1)
 	return (-15);
-	}
 	}
 }	
