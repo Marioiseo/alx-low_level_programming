0b2129066f090548671a0ab50d08e960c6ae50fa
Marioiseo <mariohany302@gmail.com>
2023-03-30 10:47:29 -0700

    NOTHING WRONG !

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 
 	int a1, a2;
 
-	for (i = 0;s1[i] != '\0' ; i++)
+	for (i = 0; ; i++)
 	{
 	if (s1[i] == '\0' && s2[i] == '\0')
 	{
@@ -21,11 +21,11 @@ int _strcmp(char *s1, char *s2)
 	a2 = (int) s2[i];
 	if (a1 > a2)
 	{
-	return (1);
+	return (15);
 	}
 	if (a2 > a1)
 	{
-	return (-1);
+	return (-15);
 	}
 	}
 }	
