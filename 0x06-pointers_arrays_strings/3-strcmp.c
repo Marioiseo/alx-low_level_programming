d0a95716b0581bf11624ef3fdedcb027de3d4879
Marioiseo <mariohany302@gmail.com>
2023-03-30 10:52:48 -0700

    wewaa

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,11 +7,15 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, j;
 
 	int a1, a2;
 
-	for (i = 0; ; i++)
+	while (s1[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i <= j ; i++)
 	{
 	if (s1[i] == '\0' && s2[i] == '\0')
 	return (0);
