9d77070b628f2350290ec3c1a037ca6974e99ff8
Marioiseo <mariohany302@gmail.com>
2023-03-30 11:14:42 -0700

    lot of mistakes

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,21 +7,12 @@
 */
 void reverse_array(int *a, int n)
 {
-	int *b;
-
 	int c = 0;
 
 	int i;
 
-	int j = 0;
-
-	while (a[j] != 0)
+	for (i = n; i >= 0; i--)
 	{
-		j++;
-	}
-	for (i = j; i >= 0; i--)
-	{
-	b[c] = a[i];
-	c++;
+	c += a[i];
 	}
 }
