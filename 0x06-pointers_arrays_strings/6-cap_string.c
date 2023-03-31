9cfc11cce25c0f4240225a76e5f6235c496ff80d
Marioiseo <mariohany302@gmail.com>
2023-03-31 09:11:44 -0700

    cant believe it

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,7 +18,7 @@ char *cap_string(char *str)
 			continue;
 		}
 		}
-		if (str[i] == ' ' || str[i] == '\n' || str[i] == ',' || str[i] == '	')
+		if (str[i] == ' ' || str[i] == '\n' || str[i] == ',' || str[i] == '\t')
 			++i;
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
