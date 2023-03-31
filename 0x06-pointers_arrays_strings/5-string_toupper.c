8180a610cb5dac06a97b141bf17d389eaae0b843
Marioiseo <mariohany302@gmail.com>
2023-03-31 07:57:32 -0700

    i wish its that easy

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - to upper case i guess we've done it before
+ * @str: the string isnt called lol
+ * Return: char it is
+*/
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	char c;
+
+	while (str[i] != '\0')
+	{
+	c = str[i];
+	c = c - 32;
+	str[i] = c;
+	}
+}
