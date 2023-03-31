3ffe24af24edfdd8dfd6eca19e8f80e69ed6251c
Marioiseo <mariohany302@gmail.com>
2023-03-31 09:05:36 -0700

    yup git add .

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,32 +11,26 @@ char *cap_string(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i == 0)
-		{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] = str[i] - 32;
 			continue;
 		}
-		}
-		if (str[i] == ' ' && str[i] == '\n' && str[i] == ',' && str[i] == '	')
-		{
+		if (str[i] == ' ' || str[i] == '\n' || str[i] == ',' || str[i] == '	')
 			++i;
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] = str[i] - 32;
 			continue;
 		}
-		}
-		if (str[i] == '.' && str[i] == '?' && str[i] == '!' && str[i] == '"')
-		{
+		if (str[i] == '.' || str[i] == '?' || str[i] == '!' || str[i] == '"')
 			++i;
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] = str[i] - 32;
 			continue;
 		}
-		}
-		if (str[i] == '{' && str[i] == '}' && str[i] == '(' && str[i] == ')')
+		if (str[i] == '{' || str[i] == '}' || str[i] == '(' || str[i] == ')')
 			++i;
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
