63f725147fcc84d915c01c6e608b59884552b557
Marioiseo <mariohany302@gmail.com>
2023-03-31 08:28:55 -0700

    yuppy

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,11 +10,35 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
+		if (i == 0)
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] = str[i] - 32;
 		}
-	i++;
+		if (str[i] == ' ' && str[i] == '\n' && str[i] == ',' && str[i] == '	')
+			++i;
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		if (str[i] == '.' && str[i] == '?' && str[i] == '!' && str[i] == '"')
+			++i;
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		if (str[i] == '{' && str[i] == '}' && str[i] == '(' && str[i] == ')')
+			++i;
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		else
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] = str[i] + 32;
+		}
+		i++;
 	}
 	return (str);
 }
