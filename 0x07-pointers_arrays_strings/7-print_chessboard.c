31c03220da0d0fa87780ad5740110d7cf753ce27
Marioiseo <mariohany302@gmail.com>
2023-04-03 10:49:23 -0700

    lets see

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - first multi dimensional array task
+ * @a: i guess its fixed chars but who knows
+ * Return: VOID LOVELY VOID
+*/
+void print_chessboard(char (*a)[8])
+{
+	char chbo[8][8] = {{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
+	{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+	{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+	{'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
+};
+a = chbo;
+}
