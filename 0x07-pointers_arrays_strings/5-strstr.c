ec6fc893ebcc5effbaa0b81cdee2ccc78103589e
Marioiseo <mariohany302@gmail.com>
2023-04-03 09:57:17 -0700

    lets call it terror

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * _strstr - seeking for substr in original one
+ * @haystack: the original string
+ * @needle: the funny string to look after
+ * Return: the remaining string or NULL
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	int a = 0, b = 0;
+
+	char *ptr;
+
+	while (haystack[a] != '\0')
+		a++;
+	while (needle[b] != '\0')
+		b++;
+	for (i = 0; haystack[i] <= a; i++)
+	{
+		for (j = 0; needle[j] <= b; j++)
+		{
+			if (haystack[i] != needle[j])
+				break;
+			else if (haystack[i] == needle[j])
+			{
+				++j;
+			while (haystack[i+j] = needle[j])
+			{
+				j++;
+				if (needle[j] < b)
+				continue;
+			ptr = &haystack[i];
+			}
+			if (ptr == NULL)
+			{
+				break;
+			}
+			else
+				return (ptr);
+			}
+		}
+	}
+		return (NULL);
+}
