b4dd644ca5e2ce8e0474e99de5ece2c280f41caa
Marioiseo <mariohany302@gmail.com>
2023-04-03 10:54:06 -0700

    what now

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,6 +6,7 @@
 */
 void print_chessboard(char (*a)[8])
 {
+	int i, j;
 	char chbo[8][8] = {{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
 	{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
 	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
@@ -15,4 +16,11 @@ void print_chessboard(char (*a)[8])
 	{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
 	{'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
 };
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+	_putchar(chbo[i][j]);
+		}
+	}
 }
