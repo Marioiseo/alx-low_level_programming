effc7cc4b22fae514bf7ae85d56f8a9563c9ce11
Marioiseo <mariohany302@gmail.com>
2023-04-05 10:08:42 -0700

    Lovely

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,22 +4,42 @@
  * @s: string to check
  * Return: int either 1 or 0
 */
+int _strlen_recursion(char *s);
+int is_pall(char *s, int len);
+
 int is_pallindrome(char *s)
 {
-	char a;
+	return (is_pall(s, 0));
+}
+int is_pall(char *s, int len)
+{
+	int i = 0;
 
-	char b;
-
-	if (*s != '\0')
-	{
-		a = *s;
-	return (is_pallindrome(s + 1));
-	b = *s;
-	}
-	if (a == b)
+	i = _strlen_recursion(s);
+	if (i == len)
 	{
 	return (1);
 	}
+	else if (s[len] == s[i])
+	{
+	return (is_pall(s, len + 1));
+	}
 	else
+			return (0);
+}
+/**
+ * _strlen_recursion - i am still loving the recursion
+ * @s: the string to count
+ * Return: the lenght of str
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
 	return (0);
+	}
+	else
+	{
+	return  (1 + _strlen_recursion(s + 1));
+	}
 }
