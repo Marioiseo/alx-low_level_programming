37ce0899939eb05f9629cc267d62fce852dd7bc6
Marioiseo <mariohany302@gmail.com>
2023-04-05 02:32:35 -0700

    lovely

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen_recursion - i am still loving the recursion
+ * @s: the string to count
+ * Return: the lenght of str
+*/
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+	{
+	return (0);
+	}
+	else
+	{
+		i++;
+		_strlen_recursion(s + 1);
+		return (i);
+	}
+}
