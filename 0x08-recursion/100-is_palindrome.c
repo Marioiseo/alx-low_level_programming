f13b72f0a23f1455c06bcea6734baf9debf9f68f
Marioiseo <mariohany302@gmail.com>
2023-04-05 09:43:28 -0700

    first try to do it

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * is_pallindrome - this what means to be advanced lol
+ * @s: string to check
+ * Return: int either 1 or 0
+*/
+int is_pallindrome(char *s)
+{
+	char *a;
+
+	char *b;
+
+	if (*s != '\0')
+	{
+	a = s
+	return (is_pallindrome(s + 1);
+	b = s
+	}
+	else if (a == b)
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
