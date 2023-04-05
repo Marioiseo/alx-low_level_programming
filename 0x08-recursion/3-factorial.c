0867447e098c12ddfd9cdfcac5959d3af2e6013a
Marioiseo <mariohany302@gmail.com>
2023-04-05 02:46:04 -0700

    lovely

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial - n * n-1 called math
+ * @n: the num to get fac for
+ * Return: -1 for errors or int
+*/
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (factorial(n * n - 1);
+	}
+}
