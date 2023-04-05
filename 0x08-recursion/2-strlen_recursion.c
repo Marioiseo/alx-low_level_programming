add0a07b5419ff3a379c210506ed6a8efaa6b6ba
Marioiseo <mariohany302@gmail.com>
2023-04-05 02:38:58 -0700

    lovely

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,12 +10,11 @@ int _strlen_recursion(char *s)
 
 	if (*s == '\0')
 	{
-	return (i);
+	return (0);
 	}
 	else
 	{
-		i += i + 1;
-		_strlen_recursion(s + 1);
+		i += _strlen_recursion(s + 1);
 	}
-	return (0);
+	return (i);
 }
