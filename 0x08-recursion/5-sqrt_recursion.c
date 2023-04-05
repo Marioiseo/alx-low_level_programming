6a35ea05404380820949e36ad47b80f354b27bdd
Marioiseo <mariohany302@gmail.com>
2023-04-05 07:58:24 -0700

    lets see it

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - i guess this one isnt possible
+ * @n: the square num
+ * Return: int or -1 for errors
+*/
+int _sqrt_recursion(int n)
+{
+	int k = 1;
+	for (k * k == n)
+		return (1);
+	else 
+	{
+	return (_sqrt_recursion(n - 1));
+	}
+}
