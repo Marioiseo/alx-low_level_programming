534a7144c4498ebd37d6d3dff5e45d56f278c762
Marioiseo <mariohany302@gmail.com>
2023-04-08 01:48:39 -0700

    easy

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - first argument to take is the name of file as default
+ * @argc: the counter of argv
+ * @argv: a pointer to array or a array of strings ^^
+ * Return: i guess its int 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s",argv[0]);
+}
