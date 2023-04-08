532f38487b0b6d2a91da26f19198d85048adebfc
Marioiseo <mariohany302@gmail.com>
2023-04-08 02:09:51 -0700

    now its good with libraries available

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -  easy project args are fun
+ * @argc: its the counter of argv
+ * @argv: its the array of strings or arguments provided
+ * Return: 0 and more zeroos
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%i\n", argc);
+	return (0);
+}
