631d6090d9159137d79be50662df3b663d40f1a5
Marioiseo <mariohany302@gmail.com>
2023-04-09 08:57:34 -0700

    GOT IT betty 100-change.c betty 100-change.c !

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - advanced for sure
+ * @argc: 1 argument only
+ * @argv: a number to break
+ * Return: 1 nor 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b = 0;
+
+	int amount = atoi(argv[1]);
+
+	int change[] = {25, 10, 5, 2, 1};
+
+	if (argc == 2)
+	{
+		for (a = 0; a < 5; a++)
+		{
+		if (amount >= change[a])
+		{
+			b += amount / change[a];
+			amount = amount % change[a];
+			if (amount % change[a] == 0)
+			{
+				break;
+			}
+		}
+		}
+		printf("%i\n", b);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+	return (1);
+	}
+}
