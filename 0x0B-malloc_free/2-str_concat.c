34ed29a9d47aacff17f4d4d6cf9099227f4aa045
Marioiseo <mariohany302@gmail.com>
2023-04-11 08:56:49 -0700

    non sense

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,6 +15,15 @@ char *str_concat(char *s1, char *s2)
 
 	int a = strlen(s1), b = strlen(s2);
 
+	if (s1 == NULL && s2 == NULL)
+	{
+		s1 = "";
+		s2 = "";
+	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	pt = (char *) malloc(a + b + 1);
 	if (pt == NULL)
 		return (NULL);
