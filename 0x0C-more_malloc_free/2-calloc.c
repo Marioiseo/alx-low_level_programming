eb05c069aaed20dc418885b68dce0e00562028d9
Marioiseo <mariohany302@gmail.com>
2023-04-13 07:40:06 -0700

    EASY

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <main.h>
+/**
+ * _calloc - making calloc with malloc lol
+ * @nmemb: how many elements
+ * @size: how many bites
+ * Return: VOID VOID VOID
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *pt;
+	int i;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	pt = malloc(nmemb * size);
+
+	for (i = 0; i < nmemb; i++)
+	{
+		pt[i] = 0;
+	}
+	return (pt);
+}
