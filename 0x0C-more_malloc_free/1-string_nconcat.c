c6a122094ee007d79ae42ec27cba282cdc3c3102
Marioiseo <mariohany302@gmail.com>
2023-04-13 07:13:28 -0700

    seek for truth

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,25 +12,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *pt; 
 	unsigned int i, j;
-	unsigned int a = 0;
-
-	while (s1[a] != '\0')
-		a++;
 
 	if (s1 == NULL)
 		return (s2);
 	if (s2 == NULL)
 		return (s1);
 
-	for (i = 0; i < a; i++)
+	for (i = 0; i < strlen(s1); i++)
 	{
-		pt = malloc(a + n + 1);
+		pt = malloc(strlen(s1) + n + 1);
 
 		if (pt == NULL)
 			return (NULL);
+
 		pt[i] = s1[i];
 	}
-	for (j = 0, i = (a + 1); j < n; i++, j++)
+	for (j = 0 ; j < n; i++, j++)
 	{
 		if (s2[j] != '\0')
 		pt[i] = s2[j];
