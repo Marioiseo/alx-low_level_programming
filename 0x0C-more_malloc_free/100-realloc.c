4651084350c6c71339b9fdff34c0500130c9d5a0
Marioiseo <mariohany302@gmail.com>
2023-04-13 11:30:13 -0700

    COME ON git add .

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,6 +14,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	if (ptr == NULL)
 	{
 		pt = malloc(new_size);
@@ -23,11 +28,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 		return (pt);
 	}
-	if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
+
 	if (new_size > old_size)
 	{
 	pt = malloc(new_size);
