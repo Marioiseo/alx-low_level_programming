926ebe76a09a9639c650b939b940d3ad358e1b06
Marioiseo <mariohany302@gmail.com>
2023-04-13 07:33:41 -0700

    lovely

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,10 +28,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		pt[i] = s1[i];
 	}
-	for (j = 0 ; j < n; i++, j++)
+	for (j = 0 ; j < n; j++)
 	{
 		if (s2[j] != '\0')
+		{
 		pt[i] = s2[j];
+		i++;
+		}
 	}
 	pt[i] = '\0';
 	return (pt);
