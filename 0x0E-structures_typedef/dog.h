643fb2e69e7c676a94cc2bc52d6000f6b5287342
Marioiseo <mariohany302@gmail.com>
2023-04-17 10:26:09 -0700

    lol

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,10 +13,11 @@ struct dog
 	float age;
 };
 
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-#typedef struct dog dog_t;
+typedef struct dog dog_t;
 
 #endif
 
