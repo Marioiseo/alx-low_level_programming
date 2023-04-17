2df24ce326b81fd086085b7975b7798af3d52e86
Marioiseo <mariohany302@gmail.com>
2023-04-17 09:50:05 -0700

    betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - lovely dogs
+ * @name: the name of the dog
+ * @owner: the name of owner
+ * @age: the age of dog nor owner lol
+ */
 struct dog
 {
 	char *name;
