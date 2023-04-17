95694f0997dfba52ddecf50a0acc07fe5d1bc287
Marioiseo <mariohany302@gmail.com>
2023-04-17 08:04:28 -0700

    Lets go

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,10 @@
 #include "stdio.h"
-
-int main()
+/**
+ * main - famous macros to use
+ * Return: 0 because i am always right
+ */
+int main(void)
 {
-	printf("%s\n", __FILE__ );
+	printf("%s\n", __FILE__);
 	return (0);
 }
