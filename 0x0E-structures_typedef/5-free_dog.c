681b9a3c3b26903a76925357fa79a81795665b0a
Marioiseo <mariohany302@gmail.com>
2023-04-18 02:14:31 -0700

    free free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - run doggy run
+ * @d: doggy doggy
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->owner);
+		free(d->age);
+		free(d->name);
+		free(d);
+	}
+}
