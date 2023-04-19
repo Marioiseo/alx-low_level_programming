0bc9f941a6054269b53271ac8a9e2a1464091e36
Marioiseo <mariohany302@gmail.com>
2023-04-19 09:28:03 -0700

    easy

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator -function as param
+ * @array: the func
+ * @size: the size of it
+ * @action: the pointer to func
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	while (array < size)
+		action(array++);
+}
