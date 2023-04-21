8ffc10693619b0ab07f1e97980c5f2bccf40cc13
Marioiseo <mariohany302@gmail.com>
2023-04-21 09:28:13 -0700

    lonely

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - print them all
+ * @separator: -_- Mr.obvoius
+ * @n: the counter
+ * Return: void void voidyyy
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	va_list ptr;
+
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+		printf("%d%s", va_arg(ptr, int), separator);
+	va_end(ptr);
+	printf("\n");
+}
