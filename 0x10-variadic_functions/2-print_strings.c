feb6b240bf7850ce575a57f31476981ed91e7753
Marioiseo <mariohany302@gmail.com>
2023-04-21 09:47:03 -0700

    MR OBVIOUS

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - print lot of strings
+ * @separator: as usual -__
+ * @n: count of strings
+ * Return: VOIDYYY
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	va_list ptr;
+
+	va_start(ptr, n);
+	if (n)
+	{
+	for (i = 0; i < n; i++)
+	{
+		if (ptr)
+		{
+			if (i < n - 1)
+			{
+			if (separator)
+				printf("%s%s", va_list(ptr, char *), separator);
+			}
+			else
+				printf("%s", va_list(ptr, char *));
+		}
+		else
+			printf("(nil)");
+	}
+	}
+	printf("\n");
+}
