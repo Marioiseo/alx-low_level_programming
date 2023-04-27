fd04594d210e9f9c708d1256ac8c996ce505f9e2
Marioiseo <mariohany302@gmail.com>
2023-04-27 01:28:19 -0700

    linked lists are tough

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - my first linked list to be done
+ * @h: pointer to the head i guess
+ * Return: i really dont know
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nill)");
+		return (1);
+		}
+		printf("%s", h->str);
+		i++;
+	}
+	return (i);
+}
