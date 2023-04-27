cb418cdd76e1e2a0b0d0df8fcf63171be1ee89b0
Marioiseo <mariohany302@gmail.com>
2023-04-27 03:12:08 -0700

    what !

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,10 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!h->str)
+		{
 			printf("[0] (nill)");
+		return (0);
+		}
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
