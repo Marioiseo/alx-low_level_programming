44a7e9d72f84ff11aef8b8185e55923c77da2791
Marioiseo <mariohany302@gmail.com>
2023-04-27 03:00:14 -0700

    just do it

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 			printf("[0] (nill)");
 		return (1);
 		}
-		printf("[%d]%s", strlen(h->str), h->str);
+		printf("[%ld]%s", strlen(h->str), h->str);
 		i++;
 		h = h->next;
 	}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 typedef struct list_s
 {
 	char *str;
