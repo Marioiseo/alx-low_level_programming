b496683eef1c2ff41a71622cc72af12931279ab3
Marioiseo <mariohany302@gmail.com>
2023-04-28 12:39:00 -0700

    i dont really get it

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * add_node - add a new node at the begining
+ * @head: the head of the linking list
+ * @str: the new string
+ * Return: the new list
+ */
+list_t add_node(list_t **head, const char *str)
+{
+	list_t *newhead;
+
+	newhead = malloc(sizeof(list_t));
+	if (!head || !newhead)
+	return (NULL);
+	if (str)
+	{
+		newhead = strdup(str);
+			if (!newhead->str)
+			{
+				free(newhead);
+				return (NULL);
+			}
+		newhead->len = strlen(newhead->str);
+	}
+	newhead->next = *head;
+	*head = newhead;
+	return (newhead);
+}
