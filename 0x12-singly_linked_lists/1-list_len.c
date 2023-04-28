e355dbd4aae48f24a42019d81afea0df0c467400
Marioiseo <mariohany302@gmail.com>
2023-04-28 11:01:11 -0700

    lovely

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * list_len - returning num of linked
+ * @h: the linked list
+ * Returning: the size
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i;
+
+	i = h->len;
+	return (i);
+}
