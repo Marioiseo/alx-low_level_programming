ee620fa198dbef9b23ffe2b1aed523ecf3a9c32b
Marioiseo <mariohany302@gmail.com>
2023-04-30 09:46:43 -0700

    what now !

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,12 +9,11 @@ int pop_listint(listint_t **head)
 	int i;
 	listint_t *replace, *newhead;
 
+	(void)newhead;
 	if (!head)
 		return (0);
 	replace = *head;
 	i = replace->n;
 	newhead = replace->next;
-	free(replace);
-	free(head);
 	return (i);
 }
