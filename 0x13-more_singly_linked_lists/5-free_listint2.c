48f614453aae54df223067adfc0d43f638292184
Marioiseo <mariohany302@gmail.com>
2023-04-30 09:24:58 -0700

    what now !

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_listint2 - make a null header
+ * @head: the head of the list
+ * VOIDIYY
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *next, *current, *newhead;
+
+	if (!head)
+		return;
+	newhead = *head;
+	current = newhead->next;
+	newhead->next = NULL;
+	*head = newhead;
+	while (current)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
