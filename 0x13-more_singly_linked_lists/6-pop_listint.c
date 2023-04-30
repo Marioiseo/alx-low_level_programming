bc8fc080fed090db3a92087b8531c74afdba92a4
Marioiseo <mariohany302@gmail.com>
2023-04-30 09:37:21 -0700

    thats so wrong

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * pop_listint - pop the intire list
+ * @head: the head of the list
+ * Return: the n inside the head
+ */
+int pop_listint(listint_t **head)
+{
+	int i;
+	listint_t *replace;
+
+	if (!head)
+		return (0);
+	replace = *head;
+	i = replace->n;
+	free(replace);
+	free(head);
+	return (i);
+}
