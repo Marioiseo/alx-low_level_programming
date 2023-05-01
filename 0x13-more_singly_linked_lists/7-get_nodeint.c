f5777b51f3e653810ebae9cbf893950bcb4dfad6
Marioiseo <mariohany302@gmail.com>
2023-05-01 07:15:03 -0700

    lovely

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - search for specific node
+ * @head: the whole list
+ * @index: the specific node
+ * Return: the specific node
+ */
+listint_t get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *node;
+
+	node = head;
+	if (!node || !head)
+		return (NULL);
+	while (node)
+	{
+		i++;
+		node = node->next;
+		if (i == index)
+			return (node);
+	}
+	return (NULL);
+}
