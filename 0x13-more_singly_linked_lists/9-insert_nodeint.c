d2c2b9b9825c93c26bbc4dbf84116ee62b4fb9b5
Marioiseo <mariohany302@gmail.com>
2023-05-01 10:34:03 -0700

    yes

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	node = *head;
 	while (node)
 	{
-		if (i == idx)
+		if (i == idx - 1)
 		{
 			newnode->next = node->next;
 			node->next = newnode;
