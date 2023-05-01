9fd2c2c5fdd5ab05ef4ce70a874cc80e2af4cc20
Marioiseo <mariohany302@gmail.com>
2023-05-01 10:29:27 -0700

    try it

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	node = *head;
 	while (node)
 	{
-		if (i == idx - 1)
+		if (i == idx)
 		{
 			newnode->next = node->next;
 			node->next = newnode;
