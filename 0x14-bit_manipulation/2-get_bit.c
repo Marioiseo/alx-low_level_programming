04040251415e22edcfee84243b62c56b134e5828
Marioiseo <mariohany302@gmail.com>
2023-05-04 11:14:53 -0700

    lets wish a m-xmas

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,29 +7,28 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int i = 0 , j;
+	unsigned int i = 0, j;
 	char arr[1024];
 
 	if (!n)
 		return (-1);
-	do
-	{
+	do {
 		if (n % 2 == 0)
 			arr[i] = '0';
 		else
 			arr[i] = '1';
 		n /= 2;
 		i++;
-	}while (n != 0);
+	} while (n != 0);
 	for (j = 0; j < i; j++)
 	{
-		if (j == index)
-		{
-			if (arr[j] == '0')
-		       return (0);
-			if (arr[j] == '1')
+	if (j == index)
+			{
+		if (arr[j] == '0')
+			return (0);
+		if (arr[j] == '1')
 			return (1);
-		}
+			}
 	}
 	return (-1);
 }
