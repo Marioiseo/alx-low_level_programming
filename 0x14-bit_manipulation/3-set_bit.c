1bfe31009dc202a26603368cb409e256e633eda7
Marioiseo <mariohany302@gmail.com>
2023-05-04 11:23:13 -0700

    just a try

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * set_bit - set a specific bit from a byte
+ * @n: the num
+ * @index: the index
+ * Return: 1 nor 0;
+ */
+int get_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i = 0, j;
+	char arr[256];
+
+	do {
+		if (*n % 2 == 0)
+			arr[i] = '0';
+		else
+			arr[i] = '1';
+		*n /= 2;
+		i++;
+	} while (*n != 0);
+	for (j = 0; j < i; j++)
+	{
+	if (j == index)
+			{
+		arr[j] = '1';
+			return (1);
+			}
+	return (-1);
+}
