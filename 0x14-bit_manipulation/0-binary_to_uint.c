533f264940f3012fec39f3c2078cfecb3aeecb7d
Marioiseo <mariohany302@gmail.com>
2023-05-04 08:30:28 -0700

    that was hard !

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,19 +1,29 @@
 #include "main.h"
 unsigned int power(int a, int b);
 /**
- * binary_to_unit - lets seek a peaceful project
+ * binary_to_uint - lets seek a peaceful project
  * @b: the binary num
  * Return: unsigned int from binary
  */
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
-	int i;
+	int i, j = 1;
 	unsigned int sum = 0;
+	int a = 0;
 
-		for (i = 0; i < 8; i++)
+	if (!b)
+		return (0);
+	a = strlen(b);
+		for (i = a - 1; i >= 0 ; j++, i--)
 		{
-		if (b[i])
-			sum+= power(2 , i);
+		if (b[i] == '1')
+		{
+			sum+= power(2 , j);
+		}
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+		else
+			continue;
 		}
 	return (sum);
 }
@@ -25,10 +35,8 @@ unsigned int binary_to_unit(const char *b)
  */
 unsigned int power(int a, int b)
 {
-	unsigned int sum = 0;
 	if (b == 1)
 		return (1);
 	else
-	sum += a * power(a , b - 1);
-	return (sum);
-}	
+	return (a * power(a , b - 1));
+}
