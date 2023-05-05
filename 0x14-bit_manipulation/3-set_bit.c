6a9ffd27dfd8ae259225fbd9c9678e4cb9d348ff
Marioiseo <mariohany302@gmail.com>
2023-05-05 06:52:00 -0700

    not cool

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,23 +7,6 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int i = 0, j;
-	char arr[256];
-
-	do {
-		if (*n % 2 == 0)
-			arr[i] = '0';
-		else
-			arr[i] = '1';
-		*n /= 2;
-		i++;
-	} while (*n != 0);
-	for (j = 0; j < i; j++)
-	{
-	if (j == index)
-			{
-		arr[j] = '1';
-			return (1);
-			}
-	return (-1);
+	*n &= ~(1 << index);
+	return (*n);
 }
