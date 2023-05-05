add8293d50503094ebc6e5eef5d42b6a7152be7b
Marioiseo <mariohany302@gmail.com>
2023-05-05 07:22:02 -0700

    burn 1

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * get_endianness - i dont even knwo what this is !
+ * Return: 1 nor 0
+ */
+int get_endianness(void)
+{
+	unsigned long int n = 1;
+	return (*(char *)&n);
+}
