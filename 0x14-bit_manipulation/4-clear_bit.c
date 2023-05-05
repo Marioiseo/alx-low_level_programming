bc2d23cb84d28e4e9c7da832c281fa99550142e1
Marioiseo <mariohany302@gmail.com>
2023-05-05 06:55:24 -0700

    lol

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * set_bit - set a specific bit from a byte
+ * clear_bit - set a specific bit from a byte
  * @n: the num
  * @index: the index
  * Return: 1 nor 0;
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	*n &= ~(1 << index);
 	return (*n);
