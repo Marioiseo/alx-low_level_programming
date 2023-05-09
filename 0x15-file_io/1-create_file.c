d0a79722a8515d036527aa686db5f106cfb2d142
Marioiseo <mariohany302@gmail.com>
2023-05-09 08:18:04 -0700

    lets wish a M-xmas

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * create_file - create file using system calls
+ * @filename: the name of it
+ * @text_content: the text inside
+ * Return: either 1 nor -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fp;
+
+	if (!filename)
+		return (-1);
+	fp = open(filename, O_CREAT|O_WRONLY|O_TRUNC, 0600);
+	if (fp == -1)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+	write(fp, text_content, 4096);
+	close(fp);
+	return (1);
