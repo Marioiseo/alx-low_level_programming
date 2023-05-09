dd24598976b4633978937e56fb20915e64c73a4d
Marioiseo <mariohany302@gmail.com>
2023-05-09 08:42:21 -0700

    lol

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fp = open(filename, O_WRONLY | O_APPEND | O_EXCL, 0600);
+	fp = open(filename, O_WRONLY | O_APPEND | O_EXCL);
 	if (fp == -1)
 		return (-1);
 	i = strlen(text_content);
