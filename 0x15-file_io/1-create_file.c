716d5b013971e218b7ff2232ddd88a12c97f7bfb
Marioiseo <mariohany302@gmail.com>
2023-05-09 08:20:31 -0700

    just go go go

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,11 +11,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fp = open(filename, O_CREAT|O_WRONLY|O_TRUNC, 0600);
+	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fp == -1)
 		return (-1);
 	if (text_content == NULL)
-		return (1);
+		return (-1);
 	write(fp, text_content, 4096);
 	close(fp);
 	return (1);
