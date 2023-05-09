7040751ad5e505c500e498eb9f16fb3dcdc7f6eb
Marioiseo <mariohany302@gmail.com>
2023-05-09 08:37:12 -0700

    wewa

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * append_text_to_file - the name is descriping
+ * @filename: just read it
+ * @text_content: same .-.
+ * Return: either 1 for success nor -1 for failures
+ */
+int appent_text_to_file(const char *filename, char *text_content)
+{
+	int fp;
+	int i;
+
+	if (!filename)
+		return (-1);
+	fp = open(filename, O_WRONLY | O_APPEND | O_EXCL, 0600);
+	if (fp == -1)
+		return (-1);
+	i = strlen(text_content);
+	if (i == 0)
+		return (1);
+	write(fp, text_content, i);
+	close(fp);
+	return (1);
+}
