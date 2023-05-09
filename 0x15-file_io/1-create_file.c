21713aef4258a92eae9ab0ae2dea958565180c2f
Marioiseo <mariohany302@gmail.com>
2023-05-09 08:23:09 -0700

    what !

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,15 +8,17 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fp;
+	int i;
 
 	if (!filename)
 		return (-1);
 	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fp == -1)
 		return (-1);
-	if (text_content == NULL)
+	i = strlen(text_content);
+	if (!i)
 		return (1);
-	write(fp, text_content, 8200);
+	write(fp, text_content, i);
 	close(fp);
 	return (1);
 }
