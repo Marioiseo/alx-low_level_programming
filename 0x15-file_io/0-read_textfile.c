5dcf2956dd4d394cd5768b964a15a4b68f4d0911
Marioiseo <mariohany302@gmail.com>
2023-05-09 07:40:58 -0700

    lets see

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * read_textfile - read with file manipulataion
+ * @filename: the filename ^^
+ * @letters: the bla bla inside
+ * Return: num of letters it could read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp;
+	char buff[1024];
+	ssize_t i = 0;
+
+	fp = fopen(filename, "r");
+	fgets(buff, letters, fp);
+	i = strlen(buff);
+	printf("%s", buff);
+	return (i);
+}
