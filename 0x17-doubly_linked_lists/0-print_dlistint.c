462febb08a8161ceae278ddacf5ddea57388d3a6
Marioiseo <mariohany302@gmail.com>
2023-06-15 05:31:34 -0700

    betty i missed you

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,10 +1,15 @@
 #include "lists.h"
+/**
+ * print_dlistint - printing all values
+ * @h: the list to check
+ * Return: the num of nodes in list
+ */
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	int a = 0;
 
-	while(h)
+	while (h)
 	{
 	printf("%d\n", h->n);
 	h = h->next;
